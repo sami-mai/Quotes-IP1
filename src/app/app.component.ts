646fc15173b027d20f32d02857d09cc20c37ddc0
sami-mai <samirah.maison@gmail>
2018-03-18 15:28:26 +0300

    add title property

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-
+import { Quotes } from './quotes'
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'Quotes App'
+  // quotes = [
+  //   new quote()
+  // ];
 }
