82c7f9204339bf5f67c4da5723167bd6688fc044
sami-mai <samirah.maison@gmail>
2018-04-12 13:54:23 +0300

    define quote class

--- src/app/quotes.ts
+++ src/app/quotes.ts
@@ -1,3 +1,8 @@
-export class Quotes {
-  constructor(public id:number, public name:string){}
+export class Quote {
+	public showDetails:boolean;
+
+	constructor(public id:number, public quote:string, public author:string, public postedBy:string, public datePublished:Date){
+		this.showDetails=false
+
+	}
 }
