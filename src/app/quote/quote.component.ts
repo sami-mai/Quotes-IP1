1be153b02e27ac9e0faaf6c48ceef8c140717659
sami-mai <samirah.maison@gmail>
2018-04-12 14:04:04 +0300

    import quote class and create a quote instance

--- src/app/quote/quote.component.ts
+++ src/app/quote/quote.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import {Quote} from '../quote';
 
 @Component({
   selector: 'app-quote',
@@ -7,6 +8,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class QuoteComponent implements OnInit {
 
+  quotes = [
+  new Quote(1,'Don\'t cry because it\'s over, smile because it happened.','Dr. Seuss','Sami-Mai',new Date(2015,3,1)),
+  new Quote(2,'Be yourself; everyone else is already taken.','Oscar Wilde','Sami-Mai',new Date(2012,3,9)),
+  new Quote(3,'So many books, so little time.','Frank Zappa','Sami-Mai',new Date(2018,1,10)),
+  new Quote(4,'Be the change that you wish to see in the world.','Mahatma Gandhi','Sami-Mai',new Date(2007,7,14)),
+  new Quote(3,'My body knows its limits, my mind refuses to accept them.','Unknown','Sami-Mai',new Date(2018,4,10)),
+  ]
+  
   constructor() { }
 
   ngOnInit() {
