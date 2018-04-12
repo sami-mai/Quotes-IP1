897d0c8147de361cee516e47aed10fdfd24ca7f4
sami-mai <samirah.maison@gmail>
2018-04-12 13:54:00 +0300

    change title

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
-import { Quotes } from './quotes'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  title = 'Mai-Quotes';
+
+  constructor(){}
 
 }
