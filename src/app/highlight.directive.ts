6c2943e0d04b5f194e3d666d2891610c4bc62616
sami-mai <samirah.maison@gmail>
2018-04-12 15:10:28 +0300

    add typescript logic

--- src/app/highlight.directive.ts
+++ src/app/highlight.directive.ts
@@ -1,10 +1,22 @@
-import { Directive } from '@angular/core';
+import { Directive, ElementRef, Input , HostListener} from '@angular/core';
 
 @Directive({
   selector: '[appHighlight]'
 })
-export class HighlightDirective {
 
-  constructor() { }
+export class HighlightDirective {
+	constructor(private elem:ElementRef){}
+
+  @HostListener("click") onClicks(){
+    this.textDeco("highlight")
+  }
+
+  @HostListener("dblclick") onDoubleClicks(){
+    this.textDeco("None")
+  }
+
+  private textDeco(action:string){
+    this.elem.nativeElement.style.textDecoration=action;
+  }
 
 }
